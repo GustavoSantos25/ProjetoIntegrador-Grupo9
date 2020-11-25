31fe01b0557a2afaa9a7c83c3cb9dde54d6c8d54
Alex Porto <sagitariano@gmail.com>
2020-11-25 18:58:49 -0300

    subindo para apresentar

--- app/src/main/java/com/example/cinefiloslogin/MainActivity.kt
+++ app/src/main/java/com/example/cinefiloslogin/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.cinefiloslogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.cinefiloslogin.ui.login.LoginActivity
+
+//import kotlinx.android
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.set
+        val login = findViewById<Button>(R.id.btn)
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
